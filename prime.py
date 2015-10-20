57dfcae8614e2cbb4a6bce76fd598f61f2ae77f6
adiultra <adityaultra4@gmail.com>
2015-10-20 22:48:35 +0530

    Yo new prime algorithm ;) idea from 9th

---
+++ prime.py
@@ -0,0 +1,40 @@
+# our prime list is empty here
+primes = []
+
+
+def is_prime(x):
+    # fxn to check prime
+
+    # we assume no. to be prime
+    a = 1
+
+    # a loop to check divisibility with already known primes
+    for i in primes:
+        # if the num is divisible by prime known already
+        # the condition turns 0 and it breaks the loop
+        if x % i == 0:
+            a = 0
+            break
+        # also breaks the loop if primes become greater than
+        # sqrt of that num
+        if i > int(x ** 0.5):
+            break
+
+    # if we do find a prime we add it to our collection
+    if a == 1:
+        primes.append(x)
+
+    return a
+
+# this loop simply runs the fxn to add newer primes
+for i in range(2, int(input('Till where primes: '))):
+    is_prime(i)
+
+# the output loop
+for i in range(len(primes)):
+    print(primes[i], end='\t')
+    if (i + 1) % 10 == 0:
+        print()
+
+# additional endline
+print()
