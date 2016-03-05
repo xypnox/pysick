aee228f74371afe45f34e74cf775a3afa142ff54
Aditya Ultra <adityaultra4@gmail.com>
2016-03-05 19:37:17 +0530

    Added variance and stdDeviation methods to stats. :fire:

--- math/stats.py
+++ math/stats.py
@@ -1,4 +1,19 @@
 class data:
+    """
+    Data Class:
+    This class is complete data management tool.
+    It stores raw or class(ed) data.
+
+    It has the following methods available:
+        obj.display()
+            display the data
+        obj.average()
+            Arithmetic mean
+        obj.deviation(<around>)
+            Deviation around the passed argument, default = mean
+        obj.organize(height)
+            Organize the data passed into class intervals
+    """
     def __init__(self, kind, data=0, freq=0):
         if kind == 'raw':
             self.raw = data
@@ -29,25 +44,44 @@ class data:
         elif self.kind == 'continuous' or self.kind == 'discrete':
             mean = 0
             for i in range(len(self.limits)):
-                mean += sum(self.limits[i])/2 * self.freq[i]
+                mean += sum(self.limits[i])/2 * self.freq[i]  # freq * classMid
             mean /= sum(self.freq)
             return mean
 
     def deviation(self, around='mean'):
         if around == 'mean':
-            var = self.average()
+            midvalue = self.average()
 
         if self.kind == 'raw':
             dvt = 0
             for i in self.raw:
-                dvt += abs(i - var)
+                dvt += abs(i - midvalue)
             dvt /= len(self.raw)
         elif self.kind == 'continuous':
             dvt = 0  # deviation
             for i in range(len(self.freq)):
-                dvt += self.freq[i]*abs((sum(self.limits[i])/2) - var)
+                dvt += self.freq[i]*abs((sum(self.limits[i])/2) - midvalue)
             dvt /= sum(self.freq)
         return dvt
+
+    def variance(self, around='mean'):
+        if around == 'mean':
+            midvalue = self.average()
+
+        if self.kind == 'raw':
+            var = 0
+            for i in self.raw:
+                var += (i - midvalue) ** 2
+            var /= len(self.raw)
+        elif self.kind == 'continuous':
+            var = 0  # deviation
+            for i in range(len(self.freq)):
+                var += self.freq[i] * ((sum(self.limits[i])/2) - midvalue) ** 2
+            var /= sum(self.freq)
+        return var
+
+    def standardDeviation(self, around='mean'):
+        return self.variance(around)**(1/2)
 
     def organize(self, height, order='continuous'):
         if self.kind == 'raw':
@@ -71,7 +105,6 @@ class data:
                 self.kind = 'continuous'
 
 # tests
-
 # marks = data(
 #     'dsc',
 #     [
@@ -79,7 +112,6 @@ class data:
 #     ],
 #     [2, 4, 6, 5, 3]
 # )
-#
 # marksc = data(
 #     'cont',
 #     [
@@ -88,17 +120,26 @@ class data:
 #     [2, 4, 6, 5, 3]
 # )
 
-runs = data(
+runs_india = data(
     'raw',
     [2, 4, 3, 6, 8, 13, 7, 8, 20, 5, 6, 14, 8, 9, 4, 13, 8, 22, 4, 1]
 )
 
-runs.display()
-print(runs.deviation())
-print(runs.average())
+runs_pakistan = data(
+    'raw',
+    [2, 4, 5, 6, 4, 3, 1, 5, 0, 6, 3, 5, 3, 4, 3, 5, 2, 4, 2, 4]
+)
 
-runs.organize(5)
+print('India')
+runs_india.display()
+print('mean =', runs_india.average())
+print('deviation =', runs_india.deviation())
+print('variance =', runs_india.variance())
+print('Standard Deviation =', runs_india.standardDeviation())
 
-runs.display()
-print(runs.deviation())
-print(runs.average())
+print('\nPakistan')
+runs_pakistan.display()
+print('mean =', runs_pakistan.average())
+print('deviation =', runs_pakistan.deviation())
+print('variance =', runs_pakistan.variance())
+print('Standard Deviation =', runs_pakistan.standardDeviation())
