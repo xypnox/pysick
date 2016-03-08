df674f4f8a5871fe9e4d46d8507f0bafad6f0310
Aditya Ultra <adityaultra4@gmail.com>
2016-03-08 20:35:25 +0530

    The project notetaking is going whacky :pencil: :cry: :angry:

--- learn/notes/notebook.py
+++ learn/notes/notebook.py
@@ -1,12 +1,32 @@
+import os
+
+
 class note:
-    def __init__(self, title, content, tags=[]):
+    def __init__(self, title, content='', tags=[]):
         self.title = title
+        self.content = content
         self.tags = tags
+        os.system('touch '+title+'.txt')
+        self.file = open(str(title+'.txt'), 'r+')
+        towrite = "Title : " + title + '\n'
+        if len(tags) > 0:
+            towrite += "Tags : " + tags + '\n'
+        towrite += "\n" + content
+        self.file.write(title+'\n'+repr(tags)+'\n'+content)
+
+    def display(self):
+        os.system('nano '+self.title+'.txt')
+
+    def addtag(self, tag):
+        self.tags.append(tag)
+
+    def finish(self):
+        self.file.close()
 
 
 class notebook:
     def __init__(self):
         self.notelist = []
 
-    def newnote(self, title, content, tags=[]):
+    def newnote(self, title, content='', tags=[]):
         self.notelist.append(note(title, content, tags))
