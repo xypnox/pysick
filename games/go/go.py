6823ad2b8cb93095355a6bb22c3c7f6466766532
Aditya Ultra <adityaultra4@gmail.com>
2016-04-05 20:40:29 +0530

    Added the isAlive Fxn(incomplete) and improved move fxn :smirk:

--- games/go/go.py
+++ games/go/go.py
@@ -3,15 +3,16 @@ class goBoard:
     def __init__(self, dimension):
         self.dimension = dimension
         self.board = []
-        self.generateBoard(dimension)
+        self.generateBoard(dimension, '+')
         self.moves = 0
 
-    def generateBoard(self, x):
+    def generateBoard(self, x, defvar):
         'generateBoard fxn to generate the board with +\'s'
+        self.defvar = '+'
         for i in range(x):
             line = []
             for j in range(x):
-                line.append('+')
+                line.append(defvar)
             self.board.append(line)
         self.moves = 0
 
@@ -35,14 +36,86 @@ class goBoard:
     def move(self, c):  # c -> coordinates
         try:
             c[0], c[1] = int(c[0]), int(c[1])
-            self.board[c[0]][c[1]] = 'X' if self.moves % 2 == 0 else 'O'
-            self.moves += 1
+            if self.board[c[0]][c[1]] == self.defvar:
+                self.board[c[0]][c[1]] = 'X' if self.moves % 2 == 0 else 'O'
+                self.moves += 1
+            else:
+                print("Wrong Move, A Piece Already Present")
         except IndexError:
             print('invalids : executing `rm -rf /`')
 
+    def isAlive(self, i, j):
+        if self.board[i][j] != self.defvar:
+            if 0 < i < 8 and 0 < j < 8:
+                if (
+                    self.board[i+1][j] == self.defvar or
+                    self.board[i][j+1] == self.defvar or
+                    self.board[i-1][j] == self.defvar or
+                    self.board[i][j-1] == self.defvar
+                ):
+                    return True
+            elif i == 0:
+                if j == 0:
+                    if (
+                        self.board[i+1][j] == self.defvar or
+                        self.board[i][j+1] == self.defvar
+                    ):
+                        return True
+                elif 0 < j < 8:
+                    if (
+                        self.board[i+1][j] == self.defvar or
+                        self.board[i][j+1] == self.defvar or
+                        self.board[i][j-1] == self.defvar
+                    ):
+                        return True
+                elif j == 8:
+                    if (
+                        self.board[i][j-1] == self.defvar or
+                        self.board[i+1][j] == self.defvar
+                    ):
+                        return True
+            elif i == 8:
+                if j == 0:
+                    if (
+                        self.board[i-1][j] == self.defvar or
+                        self.board[i][j+1] == self.defvar
+                    ):
+                        return True
+                elif 0 < j < 8:
+                    if (
+                        self.board[i-1][j] == self.defvar or
+                        self.board[i][j+1] == self.defvar or
+                        self.board[i][j-1] == self.defvar
+                    ):
+                        return True
+                elif j == 8:
+                    if (
+                        self.board[i][j-1] == self.defvar or
+                        self.board[i-1][j] == self.defvar
+                    ):
+                        return True
+            elif j == 0:
+                if 0 < i < 8:
+                    if (
+                        self.board[i+1][j] == self.defvar or
+                        self.board[i-1][j] == self.defvar or
+                        self.board[i][j+1] == self.defvar
+                    ):
+                        return True
+            elif j == 8:
+                if 0 < i < 8:
+                    if (
+                        self.board[i-1][j] == self.defvar or
+                        self.board[i+1][j] == self.defvar or
+                        self.board[i][j-1] == self.defvar
+                    ):
+                        return True
 
+# Main Execution like function
 if __name__ == "__main__":
     go = goBoard(9)
     while True:
         go.display()
         go.move(input("Coords : ").split(' '))
+        if go.isAlive(2, 1):
+            print('Alive')
